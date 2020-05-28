d35c975cc64b95ad1a919c99a8f8634247558a39
lochlanbrown1 <61527115+lochlanbrown1@users.noreply.github.com>
2020-05-28 19:55:14 +1000

    Home page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,80 @@
 
+function pollmore() {
+    var dots = document.getElementById("dotspoll");
+    var moreText = document.getElementById("morepoll");
+    var btnText = document.getElementById("btnpoll");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+}
+
+
+function drillmore() {
+    var dots = document.getElementById("dotsdrill");
+    var moreText = document.getElementById("moredrill");
+    var btnText = document.getElementById("btndrill");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+}
+
+function noisemore() {
+    var dots = document.getElementById("dotsnoise");
+    var moreText = document.getElementById("morenoise");
+    var btnText = document.getElementById("btnnoise");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+}
+
+function trashmore() {
+    var dots = document.getElementById("dotstrash");
+    var moreText = document.getElementById("moretrash");
+    var btnText = document.getElementById("btntrash");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+}
+function acidmore() {
+    var dots = document.getElementById("dotsacid");
+    var moreText = document.getElementById("moreacid");
+    var btnText = document.getElementById("btnacid");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+}
